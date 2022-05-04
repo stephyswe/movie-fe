443f65996608c2399d60bdb7a8b4720e73484cd8
Stephanie <stephaniiehallberg@gmail.com>
2022-05-04 13:33:18 +0200

    feat(tailwind): config and scss + design folder

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 const Home: NextPage = () => {
   return (
@@ -16,6 +16,7 @@ const Home: NextPage = () => {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+        
 
         <p className={styles.description}>
           Get started by editing{' '}
