a12aa80e1361dcad869a106ccd444728d986959f
Stephanie <stephaniiehallberg@gmail.com>
2022-05-11 09:19:31 +0200

    feat(admin-panel): movies w/ upload and slug field

---
+++ app/components/ui/form-elements/UploadField/UploadField.tsx
@@ -0,0 +1,44 @@
+import cn from 'classnames'
+import Image from 'next/image'
+import { FC } from 'react'
+
+import SkeletonLoader from '../../skeleton-loader/SkeletonLoader'
+import { IUploadField } from '../form.types'
+import styles from '../form.module.scss'
+
+import { useUpload } from './useUpload'
+
+const UploadField: FC<IUploadField> = ({
+  placeholder,
+  error,
+  style,
+  image,
+  folder,
+  onChange,
+  isNoImage = false,
+}) => {
+  const { uploadImage, isLoading } = useUpload(onChange, folder)
+
+  return (
+    <div className={cn(styles.field, styles.uploadField)} style={style}>
+      <div className={styles.uploadFlex}>
+        <label>
+          <span>{placeholder}</span>
+          <input type="file" onChange={uploadImage} />
+          {error && <div className={styles.error}>{error.message}</div>}
+        </label>
+        {!isNoImage && (
+          <div className={styles.uploadImageContainer}>
+            {isLoading ? (
+              <SkeletonLoader count={1} className="w-full h-full" />
+            ) : (
+              image && <Image src={image} alt="" layout="fill" unoptimized />
+            )}
+          </div>
+        )}
+      </div>
+    </div>
+  )
+}
+
+export default UploadField
